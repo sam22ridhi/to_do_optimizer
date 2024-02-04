32758b8f4a5dc3187b30bd7062ca4f68d01a3549
samieee <87869105+sam22ridhi@users.noreply.github.com>
2024-02-04 12:16:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const inputBox = document.getElementById("input-box");
+    const priorityInput = document.getElementById("priority");
+    const completionTimeInput = document.getElementById("completion-time");
     const listContainer = document.getElementById("list-container");
-    const addButton = document.querySelector(".row button");
+    const addButton = document.getElementById("add-button");
 
     addButton.addEventListener("click", addTask);
 
@@ -10,13 +12,27 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("You must write something");
         } else {
             let li = document.createElement("li");
-            li.innerHTML = inputBox.value;
+            let taskDescription = `${inputBox.value}`;
+
+            if (priorityInput.value !== "") {
+                taskDescription += ` (Priority: ${priorityInput.value})`;
+            }
+
+            if (completionTimeInput.value !== "") {
+                taskDescription += ` (Completion Time: ${completionTimeInput.value})`;
+            }
+
+            li.innerHTML = taskDescription;
             listContainer.appendChild(li);
+
             let span = document.createElement("span");
             span.innerHTML = "\u00d7";
             li.appendChild(span);
         }
+
         inputBox.value = "";
+        priorityInput.value = "";
+        completionTimeInput.value = "";
         saveData();
     }
 
@@ -39,3 +55,4 @@ document.addEventListener("DOMContentLoaded", function () {
             listContainer.innerHTML = storedData;
         }
     }
+});
