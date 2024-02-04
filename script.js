0da475a5683ffe3c8f2463f23b2ce1fd5451a077
samieee <87869105+sam22ridhi@users.noreply.github.com>
2024-02-04 11:10:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+    const addButton = document.querySelector(".row button");
+
+    addButton.addEventListener("click", addTask);
+
+    function addTask() {
+        if (inputBox.value === "") {
+            alert("You must write something");
+        } else {
+            let li = document.createElement("li");
+            li.innerHTML = inputBox.value;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+        }
+        inputBox.value = "";
+        saveData();
+    }
+
+    listContainer.addEventListener("click", function (e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+        }
+        saveData();
+    }, false);
+
+    function saveData() {
+        localStorage.setItem("data", listContainer.innerHTML);
+    }
+
+    function showTask() {
+        const storedData = localStorage.getItem("data");
+        if (storedData) {
+            listContainer.innerHTML = storedData;
+        }
+    }
